c7c2d9946c744a07f2cd55cbbb4307628f922779
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-06-25 14:44:49 +0000

    Added colors

--- src/script.js
+++ src/script.js
@@ -75,7 +75,32 @@ function renderScrapbook(posts) {
 }
 
 function background() {
-  const color = [];
+  const colors = [
+    "#e71d43",
+    "#ff0000",
+    "#ff3700",
+    "#ff6e00",
+    "#ffa500",
+    "#ffc300",
+    "#ffe100",
+    "#ffff00",
+    "#aad500",
+    "#55aa00",
+    "#008000",
+    "#005555",
+    "#002baa",
+    "#0000ff",
+    "#1900d5",
+    "#3200ac",
+    "#4b0082",
+    "#812ba6",
+    "#b857ca",
+    "#d03a87",
+  ];
+
+  colors.forEach(color => {
+    
+  });
 }
 
 background();
