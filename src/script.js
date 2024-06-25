dbcf815776537847291ef7792f078dd3fb479131
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-06-25 10:04:35 +0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
-
+fetch('https://scrapbook.hackclub.com/api/users/NguyễnGiaBách')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Data received:', data);
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
